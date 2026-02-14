1f684bab7380b1c7c592ebdf81dd3ea55019c190
iglesiasleonardo-o <iglesiasleonardo@outlook.com>
2026-02-14 23:18:33 +0200

    Add viewgen-core.js for dynamic HTML element generation

---
+++ public/js/shared/viewgen-core.js
@@ -0,0 +1,9 @@
+const tags = ['div', 'span', 'form', 'button', 'a', 'p', 'b', 'h1', 'h2', 'h3', 'table', 'tr', 'td'];
+
+const HTMLElements = {};
+
+tags.forEach(tag => {
+    HTMLElements[tag] = (attributes) => Component(tag, attributes);
+});
+
+export const { div, span, button, h1 } = HTMLElements;
